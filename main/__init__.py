e971c147c69b912f1eb99700204b50a5cb0cca54
nerudxlf <nerudxlf@gmail.com>
2021-04-17 18:07:30 +0600

    initial coomit

---
+++ main/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from configparser import SafeConfigParser
+
+config = SafeConfigParser()
+config.read('config.ini')
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = config.get("FILES", "DATABASE")
+app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpeg', 'jpg'])
+app.secret_key = config.get("DEFAULT", "SECRET_KEY")
+db = SQLAlchemy(app)
+manager = LoginManager(app)
+
+from main import routs
+
+db.create_all()
